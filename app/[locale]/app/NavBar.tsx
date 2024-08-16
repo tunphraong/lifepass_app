d6ad01944bf5f0e57ac43a35de8a344ad123ef56
Nam Pham <nampham0519@gmail.com>
2024-08-16 22:59:03 +0700

    redirect to login after log out in navbar

--- app/[locale]/app/NavBar.tsx
+++ app/[locale]/app/NavBar.tsx
@@ -67,7 +67,7 @@ Readonly<{
     try {
       const { error } = await supabase.auth.signOut();
       if (error) throw error;
-      router.push("/app/");
+      router.push("/app/login");
     } catch (error) {
       console.error("Error logging out:", error);
     }
